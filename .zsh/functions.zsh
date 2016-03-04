d07cd13106660cadeeadcb9d07be22aa5843cdc2
sgr-ksmt <melodydance.k.s@gmail.com>
2016-03-04 15:25:15 +0900

    git-remote-add-upstreamを追加

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,3 +1,22 @@
+
+git-remote-add-upstream() {
+  if ! type jq > /dev/null 2>&1; then
+    echo "'jq' is not installed." >&2
+    exit 1
+  fi
+  if [ $# -ne 1 ]; then
+    echo "Need repo name. e.g : 'user/repo'"
+    exit 1
+  fi
+  local upstream=$(curl -L "https://api.github.com/repos/$1" | jq -r '.parent.full_name')
+  if [ "$upstream" = "null" ]; then
+    echo "upstream not found."
+    exit 1
+  fi
+  git remote add upstream "git@github.com:${upstream}.git"
+}
+alias grau='git-remote-add-upstream'
+
 ## new-script template
 ## http://qiita.com/blackenedgold/items/c9e60e089974392878c8
 new-script() {
@@ -83,7 +102,7 @@ Pod::Spec.new do |s|
       Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
       The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    LICENSE
+LICENSE
   }
 
 end
