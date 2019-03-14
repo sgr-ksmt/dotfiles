117b4c2a7071e8ea1c80fd86b457c7c12971f6de
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-03-14 17:29:16 +0900

    remove unneeded code.

--- .zsh/60_zplugrc.zsh
+++ .zsh/60_zplugrc.zsh
@@ -6,16 +6,6 @@ zplug "k4rthik/git-cal", as:command, frozen:1
 
 zplug "b4b4r07/enhancd", use:"*.sh"
 
-zplug 'b4b4r07/zsh-history', as:command, use:misc/fzf-wrapper.zsh, rename-to:ff
-if zplug check 'b4b4r07/zsh-history'; then
-  export ZSH_HISTORY_FILE="$HOME/.zsh_history.db"
-  ZSH_HISTORY_KEYBIND_GET_BY_DIR="^r"
-  ZSH_HISTORY_KEYBIND_GET_ALL="^r^a"
-  ZSH_HISTORY_KEYBIND_SCREEN="^r^r"
-  ZSH_HISTORY_KEYBIND_ARROW_UP="^p"
-  ZSH_HISTORY_KEYBIND_ARROW_DOWN="^n"
-fi
-
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
     if read -q; then
